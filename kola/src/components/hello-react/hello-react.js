67dad98040c8865bd17125154d849ba519d865bd
HeiLiu <969403750@qq.com>
2018-08-07 19:58:10 +0800

    配置stylus

--- kola/src/components/hello-react/hello-react.js
+++ kola/src/components/hello-react/hello-react.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './hello.styl'
 
 class HelloWorld extends Component {
   render() {
     return (
       <div>
         <h1>Hello react, hello Kola</h1>
+        <p className="hello">你好</p>
       </div>
     )
   }
