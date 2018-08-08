9b70e38196de15ebbcf7c6623146ea97c50e8478
HeiLiu <969403750@qq.com>
2018-08-08 23:52:37 +0800

    添加加载、头部公共组件

--- kola/src/App.js
+++ kola/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import './assets/stylus/font.styl'
 import './assets/stylus/reset.styl'
 import HelloWorld from './components/hello-react/hello-react'
+import Tabbar from './common/tabbar/Tabbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HelloWorld/>
+        <Tabbar/>
       </div>
     );
   }
