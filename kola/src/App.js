1098b7742bd9537962d786362d1fd745c8a0eb95
HeiLiu <969403750@qq.com>
2018-08-09 19:01:04 +0800

    头部公共模块

--- kola/src/App.js
+++ kola/src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import './assets/stylus/font.styl'
 import './assets/stylus/reset.styl'
-import HelloWorld from './components/hello-react/hello-react'
+import Header from './common/header/Header'
 import Tabbar from './common/tabbar/Tabbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HelloWorld/>
+        <Header index scan msg/>
         <Tabbar/>
       </div>
     );
--- kola/src/common/header/Header.js
+++ kola/src/common/header/Header.js
@@ -1,24 +1,41 @@
+/**
+ * 参数： 
+ * back: 控制返回按钮的显示
+ * title: 配置header中的title
+ * sacn: 扫一扫
+ */
 import React, { Component } from 'react'
+import scan from "../../assets/images/scan.png"
+import msg from "../../assets/images/msg.png"
 import './header.styl'
 
-class MusicPlayer extends Component {
+class Header extends Component {
   handleClick() {
     // 原生的足以
     window.history.back()
   }
   render() {
     return (
-      <div className="music-header">
-        <span className="header-back" onClick={this.handleClick}>
-        {/* 图标字体 */}
-          <i className="icon-back"></i>
-        </span>
+      <div className="kola-header">
+        {
+          this.props.scan && <img className="header-scan" src={scan} alt=""/>
+        }
+        {
+          this.props.back && <span className="header-back" onClick={this.handleClick}><i className="icon-back"></i></span>
+        }
         <div className="header-title">
           {this.props.title}
         </div>
+        {
+          this.props.index && <div className="searchbanner"><i class="u-searchicon"></i><span id="j-searchbanner">周生生七夕礼 最高减1000元</span></div>
+        }
+        {
+          this.props.msg && <img className="header-msg" src={msg} alt=""/>
+        }
       </div>
+
     )
   }
 }
 
-export default MusicPlayer
+export default Header
