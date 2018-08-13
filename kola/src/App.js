852171097fd0441eaf2f9a72b4e80e777e1ca989
HeiLiu <969403750@qq.com>
2018-08-13 23:48:36 +0800

    添加carousel 和 promise

--- kola/src/App.js
+++ kola/src/App.js
@@ -13,6 +13,11 @@ class App extends Component {
         <Header scan index msg/>
         <div className="content">
           <Carousel/>
+          <article class="promise">
+            <a class="item" href="https://weex.kaola.com/activity/pages/quality.html?kpm=MjAxOA%3D%3D.MQ%3D%3D.bmV3Y29tZXI%3D.1" style={{width: '100%',height: '50px'}}>
+              <img alt="" className="promise_img" src="https://haitao.nos.netease.com/gr4TfN5hhFhFg1CaUgya1mdkPDLpE4WTT1806271501_1125_144.png?imageView&amp;type=webp&amp;thumbnail=1125x144&amp;quality=95"/>
+            </a>
+          </article>
         </div>
         <Tabbar/>
       </div>
