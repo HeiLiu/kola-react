fbee29c991169d6d3f20e82b50fee56b8fd7086f
HeiLiu <969403750@qq.com>
2018-08-18 23:25:10 +0800

    catagories

---
+++ kola/src/components/catagories/Catagories.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './catagories.styl'
+
+class Catagories extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Catagories</h1>
+        <p className="hello">显示商品分类菜单</p>
+      </div>
+    )
+  }
+}
+export default Catagories
